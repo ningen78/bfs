68eaabe20bea2db49bbacbf6bbacb81df912533d
ningen78 <ningen.78@gmail.com>
2021-12-29 14:45:09 +0100

    Initial commit

---
+++ bfs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace bfs;
+
+public static class Program
+{
+	private static void Main(string[] args)
+	{
+		var start = DateTime.Now;
+		Console.WriteLine($"Begin: {start:yyyy-MM-dd HH:mm:ss}");
+		switch (args.Length)
+		{
+			case < 1 or >= 3:
+				PrintHelp();
+				break;
+			case 1:
+				Searcher.Search(args[0]);
+				break;
+			case 2:
+				Searcher.Search(args[1], args[0]);
+				break;
+		}
+
+		var end = DateTime.Now;
+		Console.WriteLine($"Ran for: {end - start:G}");
+	}
+	
+	private static void PrintHelp()
+	{
+		Console.WriteLine("bsf [start directory] filename part");
+	}
+}
---
+++ bfs/Searcher.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace bfs;
+
+public static class Searcher
+{
+	public static void Search(string namePart, string directory)
+	{
+		Console.WriteLine($"Searching for '{namePart}' in {directory} and below");
+		foreach (var foundFile in Searcher.FoundFiles(namePart, directory))
+		{
+			Console.WriteLine(foundFile);
+		}
+	}
+
+	public static void Search(string namePart)
+	{
+		var curDir = Directory.GetCurrentDirectory();
+		Search(namePart, curDir);
+	}
+
+	public static IEnumerable<string> FoundFiles(string namePart, string directory)
+	{
+		var found = new ConcurrentQueue<string>();
+		Parallel.ForEach(Directory.GetDirectories(directory), dir =>
+		{
+			if (!Directory.Exists(dir))
+				return;
+			try
+			{
+				Parallel.ForEach(FoundFiles(namePart, dir), fn => found.Enqueue(fn));
+			}
+			catch
+			{
+				// ignored
+			}
+		});
+		try
+		{
+			Parallel.ForEach(Directory.GetFiles(directory, namePart), fn => { Console.WriteLine(fn); });
+		}
+		catch
+		{
+			// ignored
+		}
+
+		return found.ToArray();
+	}
+}
